3265c38ecd60f9e0122f5dff286398f5f724f923
raugustin <guus@kangoos.demon.nl>
2017-06-05 00:36:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-
+<?php
+$Get_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$parts=(parse_url($Get_url));
+$params=explode("/", ($parts["path"]));
+$controller="/var/www/html/autoplanet/application/controllers/".$params[1].".php";
+if (($Get_url=="https://www.autoplanet.nu/") OR ($Get_url=="https://www.autoplanet.nu/home")) {   
+require_once('/var/www/html/autoplanet/application/controllers/searchcar.php');}
+if (file_exists($controller)) {
+require_once($controller); }
+else { 
+require_once('/var/www/html/autoplanet/application/controllers/searchcar.php');
+}
+?>
