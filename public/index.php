f30b94da221c2a4a54a209424e422851bbed33db
Rob Augustinus <guus@kangoos.demon.nl>
2017-06-17 16:45:39 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 $Get_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $parts=(parse_url($Get_url));
 $params=explode("/", ($parts["path"]));
-// Make controller file name based on URL path
+// Compose controller file name based on URL path
 $controller="/var/www/html/autoplanet/application/controllers/".$params[1].".php";
 if (($Get_url=="https://www.autoplanet.nu/") OR ($Get_url=="https://www.autoplanet.nu/home")) {   
 require_once('/var/www/html/autoplanet/application/controllers/searchcar.php');}
